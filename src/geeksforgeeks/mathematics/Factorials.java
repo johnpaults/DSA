6404cdb55a78cf490686c4a8e045c25a584a2e9c
johnpaults <tsjohnpaul92@gmail.com>
2021-11-06 12:26:17 +0530

    Basic Factorials

--- src/geeksforgeeks/mathematics/Factorials.java
+++ src/geeksforgeeks/mathematics/Factorials.java
@@ -1,11 +1,6 @@
 package geeksforgeeks.mathematics;
 
 public class Factorials {
-    public static void main(String[] args) {
-        System.out.println(Factorials.findFact(10));
-        System.out.println(Factorials.findFactorialsRecrusion(10));
-    }
-
     public static int findFact(int num) {
         int fact = 1;
         for (int i = 2; i <= num; i++) {
--- src/geeksforgeeks/mathematics/Mathematics.java
+++ src/geeksforgeeks/mathematics/Mathematics.java
@@ -4,5 +4,7 @@ public class Mathematics {
     public static void main(String ...args) {
         System.out.println("Number of digits of the number 764647223 is " + FindDigit.numberOfDigit(764647223));
         System.out.println("Palidrome of 100001 is " + Palindrome.isPalindromeNumber(100001));
+        System.out.println("Factorials of 5 is " + Factorials.findFact(5));
+        System.out.println("Factorials of 5 using recursion is " + Factorials.findFactorialsRecrusion(5));
     }
 }

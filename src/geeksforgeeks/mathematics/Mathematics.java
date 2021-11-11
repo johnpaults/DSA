bbce95ede301066b06694d269e65e5adddd4baa1
johnpaults <tsjohnpaul92@gmail.com>
2021-11-11 20:39:47 +0530

    spell check

--- src/geeksforgeeks/mathematics/Mathematics.java
+++ src/geeksforgeeks/mathematics/Mathematics.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 public class Mathematics {
     public static void main(String ...args) {
         System.out.println("Number of digits of the number 764647223 is " + FindDigit.numberOfDigit(764647223));
-        System.out.println("Palidrome of 100001 is " + Palindrome.isPalindromeNumber(100001));
+        System.out.println("Palindrome of 100001 is " + Palindrome.isPalindromeNumber(100001));
         System.out.println("Factorials of 100 is " + Factorials.findFact(10));
         System.out.println("Factorials of 5 using recursion is " + Factorials.findFactorialsRecrusion(10));
         System.out.println("Trailing no of zeros in factorials of 100 is " + new TrailingZerosOfFactorials().findTailingZeroOfFact(10));
